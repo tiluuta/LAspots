5801d6259a283dc9af62ce7744936ae62501c502
theirsandman <122835669+theirsandman@users.noreply.github.com>
2023-11-21 17:16:15 -0800

    Add files via upload

---
+++ sign-up-spots.php
@@ -0,0 +1,51 @@
+
+<?php
+$host = "webdev.iyaserver.com";
+$userid = "sandmanl";
+$userpw = "Ace-sweden-sonority89!";
+$db = "sandmanl_la_spots";
+
+
+$mysql = new mysqli(
+    $host,
+    $userid,
+    $userpw,
+    $db
+);
+
+if($mysql->connect_errno) {
+    echo "db connection error : " . $mysql->connect_error;
+    exit();
+}
+?>
+<link rel="stylesheet" href="stylesheet.css">
+<html>
+<head>
+    <title>Create an Account!</title>
+    <link rel="icon" type="image/x-icon" href="Assets/favicon.ico">
+</head>
+<body>
+<?php
+include './navbar.php';
+echo $navbar;
+?>
+
+
+<h1 align="center">create an account!</h1>
+
+<?php
+$signup = '<link rel="stylesheet" href="stylesheet.css">
+    <form class="login-box" action="sign-up-confirm-spots.php">
+        <div class="input">Username<br>
+            <input type="text" placeholder="Create username" name="username"></div><br><br>
+        <div class="input">Password<br>
+            <input type="text" placeholder="Create password" name="password"><br><br></div>
+        <button type="submit" class="round-button brown">Create Account</button>
+    </form>';
+
+echo '<div class="sidebar" align="center">', $signup, '</div>';
+?>
+
+</body>
+
+</html>
